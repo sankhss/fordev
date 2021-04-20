c127c74b82c0ee3dab8035f4ae8b9ae77e577287
sankhss <samuelh.dev@gmail.com>
2021-04-20 18:14:04 -0300

    ensure HttpAdapter post returns data on 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -11,13 +11,16 @@ class HttpAdapter {
 
   HttpAdapter(this.client);
 
-  Future<void> request({@required String url, @required String method, Map body}) async {
+  Future<Map> request(
+      {@required String url, @required String method, Map body}) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
-    await client.post(Uri(path: url), headers: headers, body: jsonBody);
+    final response =
+        await client.post(Uri(path: url), headers: headers, body: jsonBody);
+    return jsonDecode(response.body);
   }
 }
 
@@ -34,6 +37,12 @@ void main() {
   });
   group('post', () {
     test('Should post with correct values', () async {
+      when(client.post(
+        any,
+        headers: anyNamed('headers'),
+        body: anyNamed('body'),
+      )).thenAnswer((_) async => Response('{"any":"any"}', 200));
+
       await sut.request(url: url, method: 'post', body: {'any': 'any'});
 
       verify(client.post(
@@ -47,12 +56,28 @@ void main() {
     });
 
     test('Should post without body', () async {
+      when(client.post(
+        any,
+        headers: anyNamed('headers'),
+      )).thenAnswer((_) async => Response('{"any":"any"}', 200));
+
       await sut.request(url: url, method: 'post');
 
       verify(client.post(
         any,
         headers: anyNamed('headers'),
       ));
+    });
+
+    test('Should return data on 200', () async {
+      when(client.post(
+        any,
+        headers: anyNamed('headers'),
+      )).thenAnswer((_) async => Response('{"any":"any"}', 200));
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, {'any': 'any'});
     });
   });
 }
