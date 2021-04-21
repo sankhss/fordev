cc31ee784a5f1d60108e741aa95c0e003e934c69
sankhss <samuelh.dev@gmail.com>
2021-04-20 22:12:38 -0300

    ensure HttpAdapter post returns ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -95,5 +95,12 @@ void main() {
 
       expect(future, throwsA(HttpError.unauthorized));
     });
+
+    test('Should throw ServerError on 500', () async {
+      mockResponse(500);
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
