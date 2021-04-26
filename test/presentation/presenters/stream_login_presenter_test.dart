c90cecb6a45f4d3d35a4a94897e5e051be2be398
sankhss <samuelh.dev@gmail.com>
2021-04-26 19:01:47 -0300

    create Validation interface

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -5,9 +5,7 @@ import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
-abstract class Validation {
-  String validate({@required String field, @required String value});
-}
+import 'package:fordev/presentation/protocols/validation.dart';
 
 class LoginState {
   String emailError;
