1e7179be70c4da91405b1fbb59c5ffe1d331c2a0
sankhss <samuelh.dev@gmail.com>
2021-04-27 15:47:31 -0300

    create EmailValidation in validators

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,24 +1,10 @@
 import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 
-import 'package:fordev/validation/protocols/protocols.dart';
-
-class EmailValidation implements FieldValidation {
-  final String field;
-
-  EmailValidation(this.field);
-
-  @override
-  String validate(String value) {
-    final regex = RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
-    final isValid = value == null || value.isEmpty || regex.hasMatch(value);
-    return isValid ? null : 'This is not a valid email.';
-  }
-}
+import 'package:fordev/validation/validators/validators.dart';
 
 void main() {
   EmailValidation sut;
-  String email;
 
   setUp((){
     sut = EmailValidation('any');
