07ad49085ab7ec2fc535a9bd382ab4df11bc3094
sankhss <samuelh.dev@gmail.com>
2021-04-27 15:43:25 -0300

    ensure EmailValidation returns null if value is valid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 
 import 'package:fordev/validation/protocols/protocols.dart';
@@ -9,15 +10,19 @@ class EmailValidation implements FieldValidation {
 
   @override
   String validate(String value) {
-    return null;
+    final regex = RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+    final isValid = value == null || value.isEmpty || regex.hasMatch(value);
+    return isValid ? null : 'This is not a valid email.';
   }
 }
 
 void main() {
   EmailValidation sut;
+  String email;
 
   setUp((){
     sut = EmailValidation('any');
+    email = faker.internet.email();
   });
 
   test('Should return null if email is empty', () {
@@ -26,5 +31,9 @@ void main() {
 
   test('Should return null if email is null', () {
     expect(sut.validate(null), null);
+  });
+
+  test('Should return null if email is valid', () {
+    expect(sut.validate(email), null);
   });
 }
