d62967d5e154e616831f920ae1d072f799130470
sankhss <samuelh.dev@gmail.com>
2021-04-28 17:00:57 -0300

    ensure LocalStorageAdapter throws save secure errors

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -36,4 +36,12 @@ void main() {
 
     verify(secureStorage.write(key: key, value: value));
   });
+
+  test('Should throw if save secure throws error', () async {
+    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value'))).thenThrow(Exception());
+    
+    final future = sut.saveSecure(key: key, value: value);
+
+    expect(future, throwsA(TypeMatcher<Exception>()));
+  });
 }
